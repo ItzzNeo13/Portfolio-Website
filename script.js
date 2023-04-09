f1fc69c7da787e30eb20fe0c94c08fa4c4d3f500
Prasad <ghaisasprasad32@gmail.com>
2023-04-09 11:07:48 +0530

    Portfolio tab update.
    Added new designs to script images array and made page more responsive for small screens (mobiles)

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var images = [  'https://www.dropbox.com/s/memt4mc71ubv015/Credit-Card-checkout.jpg?raw=1',  
     'https://www.dropbox.com/s/2vmdtuw95j5uxyc/Facebook-UI.png?raw=1',       
-    'https://www.dropbox.com/s/gprtddc10oev7rk/Software-business-card.png?raw=1',  
+    'https://www.dropbox.com/s/gprtddc10oev7rk/Software-business-card.png?raw=1',
+    'https://www.dropbox.com/s/kcfv27c0s91cw51/Portfolio.png?raw=1',
+    'https://www.dropbox.com/s/m1p5nwcmz2ngn3c/ho-deliveries.png?raw=1',
+    'https://www.dropbox.com/s/wpm5b1jlj2o1a76/brooklyn-club-ad.png?raw=1',
+    'https://www.dropbox.com/s/nxf56tnfqtjqlqz/ganesh-chaturthi-wishes.png?raw=1',  
     'https://www.dropbox.com/s/723uznv5afki0gl/Thank-you.png?raw=1'];
-
 var num = 0;
 var imgElements = images.map(function(src) {
   var img = new Image();
